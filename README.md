2694ecb58dde42c572cf89e73ef9798a97681230
pdriding <peterdriding@gmail.com>
2023-08-02 12:25:16 +0400

    Trying to get API to work

--- README.md
+++ README.md
@@ -32,22 +32,25 @@ The game engine has been compiled down to a binary so that you can run it on any
 
 To run the server, locate your operating system and run the associated command in your terminal at the root of the project.
 
-| Your OS               | Command to start the API                                  |
-| --------------------- | --------------------------------------------------------- |
-| Mac                   | `ORIGIN_ALLOWED=http://localhost:3000 ./bin/server-darwin-amd64`   |
-| Windows               | `ORIGIN_ALLOWED=http://localhost:3000 ./bin/server-windows-amd64.exe`   |
+| Your OS | Command to start the API                                              |
+| ------- | --------------------------------------------------------------------- |
+| Mac     | `ORIGIN_ALLOWED=http://localhost:3000 ./bin/server-darwin-amd64`      |
+| Windows | `ORIGIN_ALLOWED=http://localhost:3000 ./bin/server-windows-amd64.exe` |
+
+'set ORIGIN_ALLOWED=http://localhost:3000; .\bin\server-windows-amd64.exe'
 | Linux (Ubuntu, etc..) | `ORIGIN_ALLOWED=http://localhost:3000 ./bin/server-linux-amd64` |
 
 Note that this process will use your terminal tab, so you will have to open a new tab and navigate back to the project root to start the front end.
 
 #### WINDOWS USERS -- Setting Environment Variables
+
 If you are using a windows machine:
-1. `cd` into the root of the project containing data.json 
+
+1. `cd` into the root of the project containing data.json
 2. Run the following command to add the environment variable:
-```set DATA_FILE=./data.json```
+   `set DATA_FILE=./data.json`
 
 If you still run into issues running the API server on your machine, you can run this project in the Udacity classroom.
-
 
 ### Start the Frontend
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // setup the ability to see into response bodies
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // setup the express assets path
-app.use('/', express.static(path.join(__dirname, '../client')))
+app.use("/", express.static(path.join(__dirname, "../client")));
+
+// app.use(cors());
 
 // API calls ------------------------------------------------------------------------------------
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/pages/home.html'));
-})
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/pages/home.html"));
+});
 
-app.get('/race', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/pages/race.html'));
-})
+app.get("/race", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/pages/race.html"));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.get("/api/tracks", (req, res) => {
+//   // Your logic to retrieve and return tracks data
+//   return res.json();
+// });
+
+// app.get("/api/tracks", async (req, res) => {
+//   // Your logic to fetch tracks data from a data source or an external API
+//   // For now, let's assume some sample data
+//   const tracks = [
+//     { id: 1, name: "Track 1", length: "3:45" },
+//     { id: 2, name: "Track 2", length: "4:12" },
+//     // Add more tracks as needed
+//   ];
+
+//   // Respond with the tracks data in JSON format
+//   res.json(tracks);
+// });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
